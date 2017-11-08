d8be9595b1546e2c27431ef0dfa47657627cedb4
Olivia Boertje <malevolentninja@users.noreply.github.com>
2017-11-08 09:59:23 +0000

    Create trainingJS1_HelloWorld.js
    First Kata

---
+++ JavaScript/8kyu/trainingJS1_HelloWorld.js
@@ -0,0 +1,17 @@
+
+// First Time
+
+function helloWorld() {
+  var str = "Hello World!";
+  console.log( str );
+}
+
+// if typed helloworld here it would run the function
+
+
+// ES6 - replace var with either const or let
+
+function helloWorld() {
+  let str = "Hello World!";
+  console.log( str );
+}
