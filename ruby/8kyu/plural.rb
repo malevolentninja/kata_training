ab94be207f0cf877598e24fc26aa2da334a39e75
Olivia Boertje <malevolentninja@users.noreply.github.com>
2017-11-21 17:01:10 +0000

    Create plural.rb

---
+++ ruby/8kyu/plural.rb
@@ -0,0 +1,8 @@
+# It should take a number, and return true if a plural should be used with that number or false if not.
+# This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+
+
+def plural(n)
+  n != 1
+end
